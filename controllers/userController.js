61d42dd1b9709e7e520403f3fad1f987eb826261
Kevin Willot <kevin.willot.nawezi@gmail.com>
2021-12-23 22:23:39 +0100

    add encryption for password

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,22 +1,35 @@
 const User = require('../models/user');
 
 exports.signup = (req, res, next) => {
-	const user = new User({
-		...req.body,
-	});
-	user
-		.save()
-		.then(() => res.status(201).json({ message: 'User created successfully' }))
-		.catch((err) => res.status(400).json({ message: err }));
+	// encrypt the password before sendind data to server
+	bcrypt
+		.hash(req.body.password, 10)
+		.then((hash) => {
+			const user = new User({
+				email: req.body.email,
+				password: hash,
+			});
+			user
+				.save()
+				.then(() => res.status(201).json({ message: 'User created successfully' }))
+				.catch((error) => res.status(400).json({ error }));
+		})
+		.catch((error) => res.status(500).json({ error }));
 };
 
 exports.login = (req, res, next) => {
 	User.findOne({ email: req.body.email })
-		.then((user) => {
-			if (user.password !== req.body.password) {
-				return res.status(401).json({ message: 'Invalid password' });
-			}
-			res.status(200).json(user);
-		})
+		.then(
+			//check if the password was generated with the same based String
+			bcrypt.compare(req.body.password, user.password).then((valid) => {
+				if (!valid) {
+					return res.status(401).json({ error: 'User not found' });
+				}
+				if (user.password !== req.body.password) {
+					return res.status(401).json({ message: 'Invalid password' });
+				}
+				res.status(200).json({ message: 'User successfully logged in' });
+			})
+		)
 		.catch(() => res.status(404).json({ message: 'User not found' }));
 };
