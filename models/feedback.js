5f2eda4066f9e69d7f0703a607a8dac9d3ae8b9e
Kevin Willot <kevin.willot.nawezi@gmail.com>
2021-12-25 15:29:38 +0100

    delete TODO

--- models/feedback.js
+++ models/feedback.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 
-//TODO rating > feedback
 const feedbackSchema = mongoose.Schema({
 	rating: { type: Number, required: true },
 	note: { type: String, required: true },
