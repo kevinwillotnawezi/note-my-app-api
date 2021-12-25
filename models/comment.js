cec9c298c41bf244ebc35ee6633db0081b8946c0
Kevin Willot <kevin.willot.nawezi@gmail.com>
2021-12-25 15:25:04 +0100

    update comment model

--- models/comment.js
+++ models/comment.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
 
 const commentSchema = mongoose.Schema({
-	score: { type: Number, required: true },
 	description: { type: String, required: true },
-	user: { type: String, required: true },
-	ratingId: { type: String, required: true },
+	userId: { type: String, required: true },
+	feedbackId: { type: String, required: true },
 });
 
 module.exports = mongoose.model('Comment', commentSchema);
