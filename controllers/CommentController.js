edf8e6075f4a733edacb1f44416162be3d353cb1
Kevin Willot <kevin.willot.nawezi@gmail.com>
2022-01-03 14:31:10 +0100

    update call api

--- controllers/CommentController.js
+++ controllers/CommentController.js
@@ -1,7 +1,6 @@
 const Comment = require('../models/Comment');
 
 exports.createComment = (req, res, next) => {
-	//delete req.body._id;
 	const comment = new Comment({
 		...req.body,
 	});
@@ -11,32 +10,34 @@ exports.createComment = (req, res, next) => {
 		.catch((error) => res.status(400).json({ message: error }));
 };
 
-exports.getAllComments = (req, res, next) => {
-	Comment.find()
-		.then((comments) => res.status(200).json(comments))
-		.catch((error) => res.status(400).json({ message: error }));
-};
-
 exports.getAllCommentsByFeedbackId = (req, res, next) => {
 	Comment.find({ feedbackId: req.params.id })
 		.then((notations) => res.status(200).json(notations))
 		.catch((error) => res.status(400).json({ message: error }));
 };
 
-//Get only one comment is not used yet
+//Get all comments is not used
+exports.getAllComments = (req, res, next) => {
+	Comment.find()
+		.then((comments) => res.status(200).json(comments))
+		.catch((error) => res.status(400).json({ message: error }));
+};
+
+//Get only one comment is not used
 exports.getComment = (req, res, next) => {
 	Comment.findOne({ _id: req.params.id })
 		.then((commment) => res.status(200).json(commment))
 		.catch((error) => res.status(400).json({ message: error }));
 };
 
+//Upate comment is not used
 exports.updateComment = (req, res, next) => {
 	Comment.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
 		.then(() => res.status(200).json({ message: 'Comment updated' }))
 		.catch((error) => res.status(400).json({ message: error }));
 };
 
-//Delete a comment is not used yet
+//Delete a comment is not used
 exports.deleteComment = (req, res, next) => {
 	Comment.deleteOne({ _id: req.params.id })
 		.then(() => res.status(200).json({ message: 'Comment deleted' }))
--- controllers/notationController.js
+++ controllers/notationController.js
@@ -1,48 +1,56 @@
 const Notation = require('../models/Notation');
 
 exports.createNotation = (req, res, next) => {
-	//delete req.body._id;
 	const notation = new Notation({
 		...req.body,
 	});
-	notation
-		.save()
-		.then(() => res.status(201).json({ message: 'Notation registered successfully' }))
+
+	//Check if there is an existing notification for this user and this feedback/comment
+	Notation.findOne({ userId: notation.userId, feedbackId: notation.feedbackId, commentId: notation.commentId })
+		.then((notationFound) => {
+			if (notationFound) throw 'Existing feedback/comment';
+			notation
+				.save()
+				.then(() => res.status(201).json({ message: 'Notation registered successfully' }))
+				.catch((error) => res.status(400).json({ message: error }));
+		})
 		.catch((error) => res.status(400).json({ message: error }));
 };
 
+exports.getNumberOfNotationsByCommentId = (req, res, next) => {
+	Notation.aggregate([{ $match: { commentId: req.params.id } }, { $group: { _id: '$commentId', totalSum: { $sum: '$value' } } }])
+		.then((aggregates) => res.status(200).json(aggregates[0]?.totalSum || 0))
+		.catch((error) => res.status(400).json({ message: error }));
+};
+
+exports.getNumberOfNotationsByFeedbackId = (req, res, next) => {
+	Notation.aggregate([{ $match: { feedbackId: req.params.id } }, { $group: { _id: '$feedbackId', totalSum: { $sum: '$value' } } }])
+		.then((aggregates) => res.status(200).json(aggregates[0]?.totalSum || 0))
+		.catch((error) => res.status(400).json({ message: error }));
+};
+
+//Get all notations is not used
 exports.getAllNotations = (req, res, next) => {
 	Notation.find()
 		.then((notations) => res.status(200).json(notations))
 		.catch((error) => res.status(400).json({ message: error }));
 };
 
-exports.getNumberOfNotationsByCommentId = (req, res, next) => {
-	Notation.count({ commentId: req.params.id })
-		.then((notations) => res.status(200).json(notations))
-		.catch((error) => res.status(400).json({ message: error }));
-};
-
-exports.getNumberOfNotationsByFeedbackId = (req, res, next) => {
-	Notation.count({ feedbackId: req.params.id })
-		.then((notations) => res.status(200).json(notations))
-		.catch((error) => res.status(400).json({ message: error }));
-};
-
-//Get only one notation is not used yet
+//Get only one notation is not used
 exports.getNotation = (req, res, next) => {
 	Notation.findOne({ _id: req.params.id })
-		.then((commment) => res.status(200).json(commment))
+		.then((notation) => res.status(200).json(notation))
 		.catch((error) => res.status(400).json({ message: error }));
 };
 
+//Update notation is not used
 exports.updateNotation = (req, res, next) => {
 	Notation.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
 		.then(() => res.status(200).json({ message: 'Notation updated' }))
 		.catch((error) => res.status(400).json({ message: error }));
 };
 
-//Delete a notation is not used yet
+//Delete a notation is not used
 exports.deleteNotation = (req, res, next) => {
 	Notation.deleteOne({ _id: req.params.id })
 		.then(() => res.status(200).json({ message: 'Notation deleted' }))
